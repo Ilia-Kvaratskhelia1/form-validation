b6cc8dfdeb84a78f90627e42840ed8485f6ff387
Ilia-kvaratskhelia1 <https://github.com/Ilia-Kvaratskhelia1>
2023-02-19 06:09:49 +0400

    validation

--- src/componentss/Form.tsx
+++ src/componentss/Form.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./Form.css";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import errorPng from "../images/icon-error.svg";
 function Form() {
+  const forMail=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
   const {
     register,
     handleSubmit,
@@ -15,11 +17,13 @@ function Form() {
     EmailAddress: string;
     Password: string;
   };
-  type NewType = SubmitHandler<Inputs>;
-  const onSubmit: NewType = (data) => {
-    return console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    return (
+      console.log(data )
+
+    )
   };
-  console.log("FirstName errors", errors);
+  console.log("ერროოორ" , errors);
   return (
     <div>
       <div className="sale">
@@ -33,36 +37,61 @@ function Form() {
             type="text"
             id="FirstName"
             placeholder="First Name"
-            {...register("FirstName", { required: "errori moxda" })}
+            {...register("FirstName", 
+            {required:true , pattern:{
+            value:/^[A-Za-z]+$/i ,
+            message:"for firstName"}
+            })}
+            style={{ borderColor: errors.FirstName ? "Red" : "" }}
+
           />
-          {errors.FirstName && (
-            <img src={errorPng} className="errorStyle"></img>
-          )}
+          {errors.FirstName && <img src={errorPng} className="errorStyle" />}
         </div>
+        {errors.FirstName &&  <span style={{color:"brown" , fontSize:"0.90rem"}}>cant be a number or free and only english</span>}
+
         <div className="inputsCont">
           <input
             type="text"
             id="Last Name"
             placeholder="Last Name"
-            {...register("LastName")}
+            {...register("LastName",{required:true ,  pattern:{
+              value:/^[A-Za-z]+$/i ,
+              message:"for LastName"},
+
+              })}
+            style={{ borderColor:errors.LastName ? "Red" : "" }}
           />
+          {errors.LastName && <img src={errorPng} className="errorStyle" />}
         </div>
+        {errors.EmailAddress &&  <span style={{color:"brown" , fontSize:"0.90rem"}}>cant be a number or free and only englishh</span>}
+
         <div className="inputsCont">
           <input
             type="text"
             id="Email Address"
             placeholder="Email Address"
-            {...register("EmailAddress")}
+            {...register("EmailAddress", 
+            {required:true ,pattern:
+              {value:forMail,
+              message:"მხოლოდ ასოები"
+            }})}
+            style={{ borderColor: errors.EmailAddress ? "Red" : "" }}
           />
+          {errors.EmailAddress && <img src={errorPng} className="errorStyle" />}
         </div>
+          {errors.EmailAddress &&  <span style={{color:"brown" , fontSize:"0.90rem"}}>should finished @domain.ucoz and includes one symbol</span>}
         <div className="inputsCont">
           <input
-            type="text"
+            type="password"
             id="Password"
             placeholder="Password"
-            {...register("Password")}
+            {...register("Password", { required: "password error" })}
+            style={{ borderColor: errors.Password ? "Red" : "" }}
           />
+          {errors.Password && <img src={errorPng} className="errorStyle" />}
         </div>
+        {errors.EmailAddress &&  <span style={{color:"brown" , fontSize:"0.90rem"}}>Can't be empty</span>}
+
         <button type="submit">CLAIM YOUR FREE TRIAL</button>
         <p className="infoText">
           By clicking the button, you are agreeing to our{" "}
